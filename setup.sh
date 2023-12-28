ecf246dc8e50a59256d2f719c199da3c6f03103e
Nikhil Mehra <85126935+hackwithnick@users.noreply.github.com>
2023-12-28 16:46:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,10 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
+
+[theme]
+base="dark"
+primaryColor="#4ecb5b"
+backgroundColor="#091025"
+secondaryBackgroundColor="#1e1f5e"
 " > ~/.streamlit/config.toml
